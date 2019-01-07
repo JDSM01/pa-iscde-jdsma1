3f41a3c741c64d4b1d6ef9e3e85df7834de8f2e5
Jose <jose.messejana@gmail.com>
2019-01-07 18:27:00 +0000

    Added missing comments on one of the extension points interface

--- pt.iscte.pidesco.codegenerator/src/pa/iscde/codegenerator/extensability/CodeGeneratorFunctionAddExtension.java
+++ pt.iscte.pidesco.codegenerator/src/pa/iscde/codegenerator/extensability/CodeGeneratorFunctionAddExtension.java
@@ -2,7 +2,16 @@ package pa.iscde.codegenerator.extensability;
 
 import org.eclipse.swt.widgets.Composite;
 
+/**
+ * This extension allows whoever extends it to add new functionalities to the component
+ * @author D01
+ *
+ */
 public interface CodeGeneratorFunctionAddExtension {
 
+	/**
+	 * Place where the extension will create the new functionalities
+	 * @param viewArea area where the view components will be added
+	 */
 	void createCodeGenerationContent(Composite viewArea);
 }
